66a0f7b1b75f5fb637cecbce31ccb7674cd3bbba
kdai902 <kdai902@gmail.com>
2017-03-08 20:31:21 -0500

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,6 +1,8 @@
 # ASSIGNMENT FROM COURSERA "R PROGRAMMING" COURSE
 
-# The zip file contains 332 comma-separated-value (CSV) files containing pollution monitoring data for fine particulate matter (PM) air pollution at 332 locations in the United States. Each file contains data from a single monitor and the ID number for each monitor is contained in the file name. 
+# The zip file contains 332 comma-separated-value (CSV) files containing pollution monitoring data for fine 
+# particulate matter (PM) air pollution at 332 locations in the United States. Each file contains data from 
+# a single monitor and the ID number for each monitor is contained in the file name. 
 
 setwd("C:/Users/Karen/Desktop/Coursera R Programming")
 specdata_url <- "https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2Fspecdata.zip"
@@ -9,7 +11,11 @@ unzip("specdata.zip")
 
 list.files("specdata")
 
-# PART 1: Write a function named 'pollutantmean' that calculates the mean of a pollutant (sulfate or nitrate) across a specified list of monitors. The function 'pollutantmean' takes three arguments: 'directory', 'pollutant', and 'id'. Given a vector monitor ID numbers, 'pollutantmean' reads that monitors' particulate matter data from the directory specified in the 'directory' argument and returns the mean of the pollutant across all of the monitors, ignoring any missing values coded as NA.
+# PART 1: Write a function named 'pollutantmean' that calculates the mean of a pollutant (sulfate or nitrate) 
+# across a specified list of monitors. The function 'pollutantmean' takes three arguments: 'directory', 
+# 'pollutant', and 'id'. Given a vector monitor ID numbers, 'pollutantmean' reads that monitors' particulate 
+# matter data from the directory specified in the 'directory' argument and returns the mean of the pollutant 
+# across all of the monitors, ignoring any missing values coded as NA.
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
       files <- list.files(directory, pattern = ".cvs", full.names = TRUE)
